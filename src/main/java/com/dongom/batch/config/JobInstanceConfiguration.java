5b553bb3f55dce759cc9282ba1979c1326fb0502
dongom <dongom910@gmail.com>
2021-12-12 17:47:25 +0900

    HP -> GIT

---
+++ src/main/java/com/dongom/batch/config/JobInstanceConfiguration.java
@@ -0,0 +1,54 @@
+package com.dongom.batch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobInstanceConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobInstanceConfigurationJob() {
+        return jobBuilderFactory.get("jobInstanceConfigurationJob")
+                .start(jobInstanceConfigurationStep1())
+                .next(jobInstanceConfigurationStep2())
+                .build();
+    }
+
+    @Bean
+    public Step jobInstanceConfigurationStep1() {
+        return stepBuilderFactory.get("jobInstanceConfigurationStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step jobInstanceConfigurationStep2() {
+        return stepBuilderFactory.get("jobInstanceConfigurationStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+}
